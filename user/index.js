1981815a478c0868bfc71efff8b7bc798b2e8d0f
Nugumanov Dima <31694128+NugumanovD@users.noreply.github.com>
2024-01-17 13:11:54 +0200

    Update index.js

--- user/index.js
+++ user/index.js
@@ -4,7 +4,14 @@
     "details": [
       {
         "appID": "22YYHBY34Y.com.kuba.tetra-ios",
-        "paths": [ "NOT /e/*", "*", "/", "/setpass/123456" ]
+        "paths": ["NOT /e/*", "*", "/", "/setpass/123456"],
+        "components": [
+               {
+                  "/": "/help/*",
+                  "?": { "articleNumber": "????" },
+                  "comment": "Matches any URL with a path that starts with /help/ and that has a query item with name 'articleNumber' and a value of exactly four characters."
+               }
+             ]
       }
     ]
   }
